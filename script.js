202141d919194965ba01ed3b85a2f371dc6a00f7
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 18:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function makeid(l) {
   // write your code here
+	var res='';
+	var charlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i=0;i<l;i++){
+		res+= charlist.charAt(
+			Math.floor(
+				Math.random()*charlist.length;
+			)
+		)
+	}
+
+	return res;
+	
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
