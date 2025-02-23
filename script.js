9eb0c71dc2fce354918efa563db971be71d6bb6a
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 19:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function makeid(l) {
   // write your code here
-	var res=" ";
+	var res="";
 	var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	for(var i=0;i<l;i++){
 		res+= char_list.charAt(
